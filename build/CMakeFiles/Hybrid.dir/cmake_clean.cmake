fd31b569bcf5d50d77254268771b949009fecc88
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2024-06-11 19:38:31 +0300

    CMake Init

---
+++ build/CMakeFiles/Hybrid.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hybrid.dir/Hybrid.cpp.obj"
+  "CMakeFiles/Hybrid.dir/Hybrid.cpp.obj.d"
+  "Hybrid.exe"
+  "Hybrid.exe.manifest"
+  "Hybrid.pdb"
+  "libHybrid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hybrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Hybrid.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Hybrid.
+# This may be replaced when dependencies are built.
